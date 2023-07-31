b7482d85a07639d4a26ea2b970ea290f4235da82
Pixailz <pixailz1@gmail.com>
2023-07-31 17:14:46 +0200

    [submodule] update

--- inc/template_bonus.h
+++ inc/template_bonus.h
@@ -22,18 +22,27 @@
 int		main(void);
 
 // utils/utils.1.c
+int		ft_utils_5(void);
+
+// utils/utils.1/utils.1.c
 int		ft_utils_1(void);
 
-// utils/utils.2.c
+// utils/utils.1/utils.2.c
 int		ft_utils_2(void);
 
-// utils/utils.3.c
+// utils/utils.1/utils.3.c
 int		ft_utils_3(void);
 
-// utils/utils.4.c
+// utils/utils.1/utils.4.c
 int		ft_utils_4(void);
 
-// utils/utils.5.c
-int		ft_utils_5(void);
+// utils/utils.2.c
+int		ft_utils_6(void);
+
+// utils/utils.3.c
+int		ft_utils_7(void);
+
+// utils/utils.4.c
+int		ft_utils_8(void);
 
 #endif //TEMPLATE_H
