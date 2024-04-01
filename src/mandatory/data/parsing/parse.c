26e942c7ee37ddf88b58ad640eb7fc3d0a81beb7
Pixailz <Pixailz1@gmail.com>
2024-04-01 06:50:35 +0200

    fix ft_opt_post_parse

--- src/mandatory/data/parsing/parse.c
+++ src/mandatory/data/parsing/parse.c
@@ -6,7 +6,7 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 01:01:15 by brda-sil          #+#    #+#             */
-/*   Updated: 2024/04/01 04:28:32 by brda-sil         ###   ########.fr       */
+/*   Updated: 2024/04/01 06:49:08 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,5 @@ t_bin	parse_opts(int ac, char **av)
 	ft_optadd_cmd("usage", 0, usage);
 	ft_optadd("verbose", 'v', OPT_FLAG);
 	ft_optparse(ac, av);
-	return (ft_opt_post_parse());
+	return (ft_opt_post_parse(FT_NULL));
 }
